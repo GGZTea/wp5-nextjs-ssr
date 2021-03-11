683d146e411c1b4d4be8f2de18c8fa891cbe7b1b
wanwu <609780590@qq.com>
2021-03-11 14:44:50 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,6 @@ export default function Home() {
 
       <main className={styles.main}>
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h3>Learn &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
